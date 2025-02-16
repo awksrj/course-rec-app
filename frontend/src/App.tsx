e3a4615d8babd094bceb95b3bc7c63fe4e3969d2
ManhTran <tranducmanh23456@gmail.com>
2025-02-16 01:35:36 -0500

    Did ARR

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ChatbotComponent from "./components/Chatbot";
+import AcademicRequirements from "./UIganggang/AcademicRequirement";
 
 const App: React.FC = () => {
   return (
     <div>
       <h1>Welcome to My Chatbot</h1>
-      <ChatbotComponent />
+      <AcademicRequirements />
     </div>
   );
 };
