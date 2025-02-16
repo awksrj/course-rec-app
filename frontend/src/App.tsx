f74557095f9fc01218d55374d7d92bc995e99c23
ManhTran <tranducmanh23456@gmail.com>
2025-02-16 03:49:26 -0500

    labubu

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ChatbotComponent from "./components/Chatbot";
-import AcademicRequirements from "./UIganggang/AcademicRequirement";
+import Login from "./UIganggang/Login";
+import SignUp from "./UIganggang/SignUp";
+import SignIn from "./UIganggang/SignIn";
 
 const App: React.FC = () => {
   return (
-    <div>
-      <h1>Welcome to My Chatbot</h1>
-      <AcademicRequirements />
-    </div>
+    <Router>
+      <div>
+
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/chatbot" element={<ChatbotComponent />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
